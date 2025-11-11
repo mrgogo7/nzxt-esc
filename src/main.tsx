1db8dcebe6adfd878ede68cb1c3385afce5ebc14
mrgogo7 <mrgogo7@gmail.com>
2025-11-11 15:42:15 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Display from './ui/Display'
+import Config from './ui/Config'
 
-createRoot(document.getElementById('root')!).render(<Display />)
+const params = new URLSearchParams(window.location.search)
+const isConfig = params.has('config')
+
+createRoot(document.getElementById('root')!).render(
+  isConfig ? <Config /> : <Display />
+)
