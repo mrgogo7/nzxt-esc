fa7cde85e515f663141ab63da314525b67bc5111
mrgogo7 <mrgogo7@gmail.com>
2025-11-11 18:41:33 +0300

    Update config.tsx

--- src/config.tsx
+++ src/config.tsx
@@ -1,17 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Config from './ui/Config'
-import ConfigPreview from './ui/components/ConfigPreview'
 
-// Config ekranını ve thumbnail önizlemeyi birlikte render eder
-function ConfigPage() {
-  return (
-    <div style={{ padding: '1rem' }}>
-      <Config />
-      <hr style={{ margin: '2rem 0', borderColor: '#333' }} />
-      <ConfigPreview />
-    </div>
-  )
-}
-
-createRoot(document.getElementById('root')!).render(<ConfigPage />)
+createRoot(document.getElementById('root')!).render(<Config />)
