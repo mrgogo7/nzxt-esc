3c1dad63104ad707857e694daecf91cb6a5123ab
mRGogo <mrgogo7@gmail.com>
2025-11-15 00:50:39 +0300

    feat: Add horizontal divider before Secondary section in triple mode

--- src/ui/components/ConfigPreview.tsx
+++ src/ui/components/ConfigPreview.tsx
@@ -1133,6 +1133,9 @@ export default function ConfigPreview() {
                           </button>
                         </div>
 
+                        {/* Horizontal divider */}
+                        <hr className="settings-divider" />
+
                         {/* SECONDARY SETTINGS */}
                         <div className="setting-row">
                           <label>{t('secondaryNumberColor', lang)}</label>
